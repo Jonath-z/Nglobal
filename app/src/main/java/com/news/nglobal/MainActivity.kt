59486f5055f87124ccf8f1dcdf5f5fcc28b31316
Jonath-z <jonathanzihindula95@gmail.com>
2023-01-02 12:21:40 +0200

    fix the image display on the main activity & setup the home activity

--- app/src/main/java/com/news/nglobal/MainActivity.kt
+++ app/src/main/java/com/news/nglobal/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.news.nglobal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageView>(R.id.main_imageView).setImageResource(R.drawable.welcome_prof)
+        findViewById<Button>(R.id.button_home).setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
